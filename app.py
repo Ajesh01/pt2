059f1d88fd2f39be03d05f4b0b6ac4128c421e18
Ajesh01 <ajeshmartin01@gmail.com>
2021-11-10 22:39:24 +0530

    base html added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 
 
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/') 
 def hello():
-    return 'Hello! this is my PT2 project'
+    return render_template('index.html')
 
 
 
